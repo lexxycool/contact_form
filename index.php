7cad8ab4e1bb5780d97e2050baf3bddf8bfbcc70
Obinna Mokwe <obinnamokwe@Obinnas-MacBook-Air.local>
2021-03-04 22:58:38 -0600

    second commit

--- index.php
+++ index.php
@@ -3,13 +3,15 @@
     $msg = '';
     $msgClass = '';
 
+    
+
     // check for submit
     if(filter_has_var(INPUT_POST, 'submit')) {
             
         // get form data
-           $name = $_POST['name']; 
-           $email = $_POST['email'];
-           $message = $_POST['message']; 
+           $name = htmlspecialchars($_POST['name']); 
+           $email = htmlspecialchars($_POST['email']);
+           $message = htmlspecialchars($_POST['message']); 
 
         // check required fields 
         if(!empty($name) && !empty($email) && !empty($message)) {
@@ -17,7 +19,31 @@
 
             //check email
             if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                    
                     // passed
+                    // recipient email
+                    $toMyEmail = 'maxirexy16@yahoo.com';
+                    //email subject
+                    $subject = 'Contact Request From'. $name;
+                    // email body
+                    $body = '<h2>Contact Request</h2>
+                            <h4>Name</h4><p>'. $name. '</p>
+                            <h4>Email</h4><p>'. $email. '</p>
+                            <h4>Message</h4><p>'. $message. '</p>';
+                    // Html Email Headers
+                    $headers ='MIME-Version: 1.0' . '\r\n';
+                    $headers .='Content-type:text/html;charset=UTF-8' . '\r\n';
+                    // Additional Headers
+                    $headers .= 'From:' . $name. '<'. $email. '>'. '\r\n';  
+                    // send mail
+                    if (mail($toMyEmail,$subject,$body,$headers)) {
+                        // email sent
+                         $msg = 'email sent';
+                         $msgClass = 'alert-success';
+                    } else {
+                         $msg = 'email was not sent';
+                         $msgClass = 'alert-danger';
+                    }
                 
             } else {
                 // failed
@@ -44,32 +70,34 @@
     </head>
     <body>
          <nav class='navbar navbar-expand-lg navbar-dark bg-dark'>
-             <div class='container'>
-                <?php if($msg != ''): ?>
-                    <div class='alert <?php echo $msgClass; ?>'>
-                        <?php echo $msg; ?>
-                    </div>
-                <?php endif; ?>
-                 <div class='navbar-header'>
-                      <a class='navbar-brand' href='index.php'>Contact Form</a>
-                 </div>
+         <div class='container'>
+                  <div class='navbar-header'>
+                       <a class='navbar-brand' href='index.php'>Contact Form</a>
+                  </div>
+                 
+                  <?php if($msg != ''): ?>
+                      <div class='alert <?php echo $msgClass; ?>'>
+                           <?php echo $msg; ?>
+                      </div>
+                  <?php endif; ?>  
               </div>
          </nav>
          <form method='post' action='<?php echo $_SERVER['PHP_SELF'] ?>'>
                 <div class='form-group'>
                     <label>Name</label>
-                    <input type='text' name='name' class='form-control' value='' >
+                    <input type='text' name='name' class='form-control' value='<?php echo isset($_POST['name']) ? $name : '' ; ?>' >
                 </div>
                 <div class='form-group'>
                     <label>Email</label>
-                    <input type='text' name='email' class='form-control' value='' >
+                    <input type='text' name='email' class='form-control' value='<?php echo isset($_POST['email']) ? $email : '' ; ?>' >
                 </div>
                 <div class='form-group'>
                     <label>Message</label>
-                    <textarea name='message' class='form-control'></textarea>
+                    <textarea name='message' class='form-control' placeholder='your message here ...' ><?php echo isset($_POST['message']) ? $message : '' ;  ?></textarea>
                 </div>
                 <br>
                 <button type='submit' name='submit' class='btn btn-primary'>Submit</button>
+                <button type='reset' name='clear' class='btn btn-primary'>Clear</button>
          </form>
     </body>
 </html>
